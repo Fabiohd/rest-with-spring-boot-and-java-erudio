dc52aa4095080b04d8910b36ca8ed412dd9cc23f
fabiosousa206 <fabio15hs@hotmail.com>
2024-12-10 16:00:11 -0300

    Liberar conexão para o client web

--- 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/WebConfig.java
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/WebConfig.java
@@ -1,37 +1,30 @@
 package br.com.erudio.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-	
-	@Value("$cors.originPatterns:default")
-	private String corsOriginPatterns = "";
 
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+		
+	
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://localhost:3000", "http://localhost")
+			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+	}
+	
 	@Override
-	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-				
-		// VIA QUERY PARAM. http://localhost:8080/api/person/v1?mediaType=xml
-		
-		/*configurer.favorParameter(true)
-		.parameterName("mediaType").ignoreAcceptHeader(true)
-		.useRegisteredExtensionsOnly(false)
-		.defaultContentType(MediaType.APPLICATION_JSON)
-			.mediaType("json", MediaType.APPLICATION_JSON)
-			.mediaType("xml", MediaType.APPLICATION_XML);*/
-		
-		configurer.favorParameter(false)
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer ) {
+		configurer
+		.favorParameter(false)
 		.ignoreAcceptHeader(false)
 		.useRegisteredExtensionsOnly(false)
 		.defaultContentType(MediaType.APPLICATION_JSON)
-			.mediaType("json", MediaType.APPLICATION_JSON)
-			.mediaType("xml", MediaType.APPLICATION_XML);
-	
+		.mediaType("json", MediaType.APPLICATION_JSON);
 	}
-	
-	
+
 }
